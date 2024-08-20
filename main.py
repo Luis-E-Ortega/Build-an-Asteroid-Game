20f95dc6e1d24a54b73f44000d6a611263713cd3
Luis-E-Ortega <lorte030@gmail.com>
2024-08-20 19:30:11 -0400

    Added methods to draw and for movement, currently working on group functionality

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player1 = Player(x, y, PLAYER_RADIUS)
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    updatable.add(player1)
+    drawable.add(player1)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+        # Limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
     print(
         " Starting asteroids!\n",
         f"Screen width: {SCREEN_WIDTH}\n",
